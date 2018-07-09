c8e4fc5fa00198df86c5926e3d1b01f940463e5b
milan <milansuvajdzic24@gmail.com>
2018-07-09 23:44:59 +0200

    kreiran model

---
+++ bank/src/main/java/com/example/bank/model/User.java
@@ -0,0 +1,50 @@
+package com.example.bank.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
+	private Long id;
+	
+	@Column(columnDefinition = "CHAR(10)", unique = true)
+	private String username;
+	
+	@Column
+	private String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+}
