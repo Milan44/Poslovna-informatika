a6be7d4bd476dd78644a8b808f7291d5f64b3ede
milan <milansuvajdzic24@gmail.com>
2018-07-13 15:05:48 +0200

    .

--- bank/src/main/java/com/example/bank/controller/BankController.java
+++ bank/src/main/java/com/example/bank/controller/BankController.java
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-package com.example.bank.controller;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.bank.model.Bank;
-
-import com.example.bank.service.BankService;
-
-@RestController
-@CrossOrigin(origins = "*")
-@RequestMapping("/public/banks")
-public class BankController {
-	
-	@Autowired
-	private BankService bankService;
-	
-	
-	@RequestMapping(
-			value = "/getAll", 
-			method = RequestMethod.GET, 
-			produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Bank>  getBanks() {
-		
-		
-		return bankService.getAll();
-		
-	}
-
-}
-=======
 package com.example.bank.controller;
 
 import java.util.List;
@@ -85,4 +47,4 @@ public class BankController {
 	}
 	
 }
->>>>>>> branch 'master' of https://github.com/Milan44/Poslovna-informatika.git
+
